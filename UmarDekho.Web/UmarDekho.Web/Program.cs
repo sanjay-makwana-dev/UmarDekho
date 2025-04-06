35924d976c051ab0ad8aa4ab6c76a1f60d938883
Sanjay Makwaa <sanjaymakwana10125@gmail.com>
2025-04-06 13:35:45 +0530

    Excluded Blazor Server, added WebAssembally

--- UmarDekho.Web/UmarDekho.Web/Program.cs
+++ UmarDekho.Web/UmarDekho.Web/Program.cs
@@ -1,3 +1,4 @@
+using UmarDekho.Web.Client.Pages;
 using UmarDekho.Web.Components;
 
 namespace UmarDekho.Web
@@ -10,12 +11,16 @@ namespace UmarDekho.Web
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+                .AddInteractiveWebAssemblyComponents();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -28,7 +33,8 @@ namespace UmarDekho.Web
             app.UseAntiforgery();
 
             app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
 
             app.Run();
         }
